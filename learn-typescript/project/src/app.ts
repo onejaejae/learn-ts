da73123dac18009c3fb418ba59fd90dfa0e8be2f
조원제 <yoteamo7@naver.com>
2022-10-16 22:47:50 +0900

    최종 프로젝트 - 차트 라이브러리 타입 정의

--- learn-typescript/project/src/app.ts
+++ learn-typescript/project/src/app.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import * as Chart from 'chart.js';
+import Chart from 'chart.js';
 import {
   CovidSummaryResponse,
   CountrySummaryResponse,
@@ -43,7 +43,6 @@ function createSpinnerElement(id: string) {
 
 // state
 let isDeathLoading = false;
-const isRecoveredLoading = false;
 
 // api
 function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
@@ -189,8 +188,9 @@ async function setupData() {
   setLastUpdatedTimestamp(data);
 }
 
-function renderChart(data: any, labels: any) {
-  const ctx = $('#lineChart').getContext('2d');
+function renderChart(data: number[], labels: string[]) {
+  const lineChart = $('#lineChart') as HTMLCanvasElement;
+  const ctx = lineChart.getContext('2d');
   Chart.defaults.color = '#f5eaea';
   Chart.defaults.font.family = 'Exo 2';
   new Chart(ctx, {
